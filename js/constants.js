c224c0145d0c55cdb04d5f98796d51b153fae95a
CoffeeScriptHero <denis_kozarenko@ukr.net>
2022-06-14 18:05:00 +0300

    sounds fixes

--- js/constants.js
+++ js/constants.js
@@ -4,15 +4,15 @@ export const canvas =
     ? document.getElementById("laptop-canvas")
     : document.getElementById("canvas");
 export const ctx = canvas.getContext("2d");
-export const pickup = new Audio("./sounds/pickup.mp3");
-export const death = new Audio("./sounds/death.mp3");
-export const fumny_pickup = new Audio("./sounds/fumny-pickup.mp3");
-export const fumny_death = new Audio("./sounds/fumny-death.mp3");
-export const pendos1 = new Audio("./sounds/pendos1.mp3");
-export const pendos2 = new Audio("./sounds/pendos2.mp3");
-export const pendos3 = new Audio("./sounds/pendos3.mp3");
-export const pendos4 = new Audio("./sounds/pendos4.mp3");
-export const pendos_death = new Audio("./sounds/pendos-death.mp3");
+export const pickup = new Audio("/sounds/pickup.mp3");
+export const death = new Audio("/sounds/death.mp3");
+export const fumny_pickup = new Audio("/sounds/fumny-pickup.mp3");
+export const fumny_death = new Audio("/sounds/fumny-death.mp3");
+export const pendos1 = new Audio("/sounds/pendos1.mp3");
+export const pendos2 = new Audio("/sounds/pendos2.mp3");
+export const pendos3 = new Audio("/sounds/pendos3.mp3");
+export const pendos4 = new Audio("/sounds/pendos4.mp3");
+export const pendos_death = new Audio("/sounds/pendos-death.mp3");
 export const pendosArr = [pendos1, pendos2, pendos3, pendos4];
 
 //settings.js constants
